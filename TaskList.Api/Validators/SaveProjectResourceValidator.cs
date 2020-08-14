4c5a3269ac719717e1232a8dc75154b2411b44a8
Dmitriy <dima251200@yandex.ru>
2020-08-14 18:42:39 +0400

    Added validators for creating and updating

---
+++ TaskList.Api/Validators/SaveProjectResourceValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskList.Api.Resources;
+
+namespace TaskList.Api.Validators
+{
+    public class SaveProjectResourceValidator : AbstractValidator<SaveProjectResource>
+    {
+        public SaveProjectResourceValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+        }
+    }
+}
---
+++ TaskList.Api/Validators/SaveToDoItemResourceValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskList.Api.Resources;
+
+namespace TaskList.Api.Validators
+{
+    public class SaveToDoItemResourceValidator : AbstractValidator<SaveToDoItemResource>
+    {
+        public SaveToDoItemResourceValidator()
+        {
+            RuleFor(t => t.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(t => t.ProjectId)
+                .NotEmpty()
+                .WithMessage("'Project Id' must not be 0.");
+        }
+    }
+}
